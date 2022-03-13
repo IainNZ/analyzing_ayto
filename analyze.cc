14597bfc7b046354ecd683988866999263d947d3
Iain Dunning <iaindunning@gmail.com>
2022-03-12 19:34:33 -0500

    Season 4

---
+++ analyze.cc
@@ -0,0 +1,134 @@
+// #include <cstdio>
+// #include <algorithm>
+// #include <array>
+// #include <map>
+// #include <set>
+// #include <string>
+// #include <vector>
+
+// Efficient storage of pairings:
+// A full pairing of men and woman can be represented by N tuples.
+// There is no inherent ordering of the tuples, so let us always
+// store it with the men in the same order. If we do so, we do not
+// need to explicitly store the men at all and instead can just
+// store the woman. This reflects that there are N! pairings total,
+// for the N! of permutations of the woman with respect to a fixed
+// ordering of men.
+
+#include "season4.h"
+
+// Utility class for operating on pairings.
+struct Pairing
+{
+    std::array<Woman, NUM_MEN> women; // Indexed by men.
+
+    bool contains(const Couple c) const
+    {
+        return women[c.first] == c.second;
+    }
+
+    void print() const
+    {
+        for (size_t i = 0; i < NUM_MEN; i++)
+        {
+            printf("(%s,%s)", MAN_MAP.at(i).c_str(), WOMAN_MAP.at(women[i]).c_str());
+        }
+        printf("\n");
+    }
+
+    bool tb(std::pair<Couple, bool> tb_result) const
+    {
+        if (tb_result.second)
+            // TB couple is a PM.
+            // Pairing is valid iff contains couple.
+            return contains(tb_result.first);
+        else
+            // TB couple is not a PM.
+            // Pairing is valid iff doesn't contain couple.
+            return !contains(tb_result.first);
+    }
+
+    bool mc(std::pair<std::array<Woman, NUM_MEN>, size_t> mc_result) const
+    {
+        // If we get K lights in a week, that means a pairing is valid iff
+        // it shares exactly K couples with the MC pairing.
+        size_t num_couple_same = 0;
+        for (Man i = 0; i < NUM_MEN; i++)
+        {
+            num_couple_same += (women[i] == mc_result.first[i]);
+        }
+        return num_couple_same == mc_result.second;
+    }
+};
+
+// Prints out the number of each time each couple appears in valid pairings.
+void printCoupleCounts(const Pairing *pairings, const bool *pairing_valid, bool dropZeros)
+{
+    std::vector<std::vector<size_t>> count;
+    for (Man m = 0; m < NUM_MEN; m++)
+        count.push_back(std::vector<size_t>(NUM_WOMEN, 0));
+    for (size_t i = 0; i < MAX_PAIRINGS; i++)
+    {
+        if (!pairing_valid[i])
+            continue;
+        for (Man m = 0; m < NUM_MEN; m++)
+            count[m][pairings[i].women[m]]++;
+    }
+
+    for (Man m = 0; m < NUM_MEN; m++)
+    {
+        printf("%s\n", MAN_MAP.at(m).c_str());
+        for (Woman w = 0; w < NUM_WOMEN; w++)
+        {
+            if (count[m][w] == 0 && dropZeros)
+                continue;
+            printf("  %10s %9zu\n", WOMAN_MAP.at(w).c_str(), count[m][w]);
+        }
+    }
+}
+
+int main(void)
+{
+    // Generate all pairings.
+    Pairing *pairings = new Pairing[MAX_PAIRINGS];
+    bool *pairing_valid = new bool[MAX_PAIRINGS];
+    size_t pairing_idx = 0;
+    do
+    {
+        Man man = 0;
+        for (const auto &woman : ALL_WOMEN)
+        {
+            pairings[pairing_idx].women[man] = woman;
+            man++;
+        }
+        pairing_valid[pairing_idx] = true;
+        pairing_idx++;
+    } while (std::next_permutation(ALL_WOMEN.begin(), ALL_WOMEN.end()));
+    printf("Initial pairings %9d\n", MAX_PAIRINGS);
+
+    for (size_t round = 0; round < 10; round++)
+    {
+        auto [tb_couple, tb_are_pm] = TB_RESULTS[round];
+        int num_valid_after_tb = 0;
+        for (size_t i = 0; i < MAX_PAIRINGS; i++)
+        {
+            if (!pairing_valid[i])
+                continue;
+            pairing_valid[i] &= pairings[i].tb(TB_RESULTS[round]);
+            num_valid_after_tb += pairing_valid[i];
+        }
+        printf("After Week %2zu TB %9d\n", round + 1, num_valid_after_tb);
+
+        int num_valid_after_mc = 0;
+        for (size_t i = 0; i < MAX_PAIRINGS; i++)
+        {
+            if (!pairing_valid[i])
+                continue;
+            pairing_valid[i] &= pairings[i].mc(MC_RESULTS[round]);
+            num_valid_after_mc += pairing_valid[i];
+        }
+        printf("After Week %2zu MC %9d\n", round + 1, num_valid_after_mc);
+
+        printCoupleCounts(pairings, pairing_valid, true);
+    }
+}
---
+++ season4.h
@@ -0,0 +1,96 @@
+// Data for Season 4
+
+#include <cstdint> // uint8_t
+
+#include <array>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define NUM_MEN 10
+#define NUM_WOMEN 10
+#define MAX_PAIRINGS 3628800
+using Man = uint8_t;
+using Woman = uint8_t;
+using Couple = std::pair<Man, Woman>;
+
+// clang-format off
+
+const Man PROSPER = 0;
+const Man JOHN = 1;
+const Man GIOVANNI = 2;
+const Man ASAF = 3;
+const Man CAM = 4;
+const Man SAM = 5;
+const Man CAMERON = 6;
+const Man MORGAN = 7;
+const Man STEPHEN = 8;
+const Man TYLER = 9;
+std::array<Man, NUM_MEN> ALL_MEN = {
+    PROSPER, JOHN, GIOVANNI, ASAF, CAM, SAM,
+    CAMERON, MORGAN, STEPHEN, TYLER};
+const std::map<Man, std::string> MAN_MAP = {
+    {PROSPER, "PROSPER"}, {JOHN, "JOHN"},
+    {GIOVANNI, "GIOVANNI"}, {ASAF, "ASAF"},
+    {CAM, "CAM"}, {SAM, "SAM"},
+    {CAMERON, "CAMERON"}, {MORGAN, "MORGAN"},
+    {STEPHEN, "STEPHEN"}, {TYLER, "TYLER"}};
+
+const Woman CAMILLE = 0;
+const Woman EMMA = 1;
+const Woman KAYLEN = 2;
+const Woman FRANCESCA = 3;
+const Woman VICTORIA = 4;
+const Woman ALYSSA = 5;
+const Woman MIKALA = 6;
+const Woman JULIA = 7;
+const Woman NICOLE = 8;
+const Woman TORI = 9;
+std::array<Woman, NUM_WOMEN> ALL_WOMEN = {
+    CAMILLE, EMMA, KAYLEN, FRANCESCA, VICTORIA,
+    ALYSSA, MIKALA, JULIA, NICOLE, TORI};
+const std::map<Woman, std::string> WOMAN_MAP = {
+    {CAMILLE, "CAMILLE"}, {EMMA, "EMMA"},
+    {KAYLEN, "KAYLEN"}, {FRANCESCA, "FRANCESCA"},
+    {VICTORIA, "VICTORIA"}, {ALYSSA, "ALYSSA"},
+    {MIKALA, "MIKALA"}, {JULIA, "JULIA"},
+    {NICOLE, "NICOLE"}, {TORI, "TORI"}};
+
+const std::pair<Couple, bool> TB_RESULTS[] = {
+    {{PROSPER, TORI}, false},
+    {{JOHN, JULIA}, false},
+    {{CAMERON, MIKALA}, true},
+    {{ASAF, TORI}, false},
+    {{GIOVANNI, KAYLEN}, false},
+    {{SAM, ALYSSA}, true},
+    {{CAM, VICTORIA}, false},
+    {{GIOVANNI, JULIA}, false},
+    {{PROSPER, EMMA}, true},
+    {{CAM, JULIA}, true},
+};
+
+const std::pair<std::array<Woman, NUM_MEN>, size_t> MC_RESULTS[] = {
+    {{CAMILLE, EMMA, KAYLEN, FRANCESCA, VICTORIA,
+      ALYSSA, MIKALA, JULIA, NICOLE, TORI}, 3},
+    {{EMMA, NICOLE, KAYLEN, CAMILLE, JULIA,
+      FRANCESCA, MIKALA, ALYSSA, TORI, VICTORIA}, 3},
+    {{EMMA, VICTORIA, KAYLEN, CAMILLE, NICOLE,
+      ALYSSA, MIKALA, FRANCESCA, TORI, JULIA}, 4},
+    {{NICOLE, VICTORIA, KAYLEN, CAMILLE, EMMA,
+      ALYSSA, MIKALA, TORI, JULIA, FRANCESCA}, 4},
+    {{VICTORIA, TORI, FRANCESCA, CAMILLE, EMMA,
+      ALYSSA, MIKALA, JULIA, NICOLE, KAYLEN}, 4},
+    {{KAYLEN, EMMA, FRANCESCA, CAMILLE, VICTORIA,
+      ALYSSA, MIKALA, TORI, JULIA, NICOLE}, 4},
+    {{VICTORIA, KAYLEN, EMMA, FRANCESCA, NICOLE,
+      ALYSSA, MIKALA, TORI, JULIA, CAMILLE}, 4},
+    {{FRANCESCA, KAYLEN, NICOLE, CAMILLE, TORI,
+      ALYSSA, MIKALA, VICTORIA, JULIA, EMMA}, 2},
+    {{EMMA, VICTORIA, FRANCESCA, TORI, NICOLE,
+      ALYSSA, MIKALA, JULIA, KAYLEN, CAMILLE}, 6},
+    {{EMMA, VICTORIA, FRANCESCA, KAYLEN, JULIA,
+      ALYSSA, MIKALA, TORI, NICOLE, CAMILLE}, 10}
+};
+
+// clang-format on
